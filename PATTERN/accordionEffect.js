f98a4ec46b9ce05d935c5920057e697875d3de94
Benoit Morawiec on Mac <benmora68@gmail.com>
2021-09-25 12:30:58 +0200

    first initial commit

---
+++ PATTERN/accordionEffect.js
@@ -0,0 +1,19 @@
+﻿var accordeon = document.getElementsByClassName("accordion");
+
+for (i = 0; i < accordeon.length; i++) {
+    accordeon[i].addEventListener("click", function () {
+
+        var shown = this.classList.toggle("active");
+
+        var symbol = (this.getElementsByClassName("symbol"))[0];
+        var panel = this.nextElementSibling;
+
+        if (shown) {
+            symbol.innerHTML = "-";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } else {
+            symbol.innerHTML = "+";
+            panel.style.maxHeight = null;
+        }
+    });
+}
